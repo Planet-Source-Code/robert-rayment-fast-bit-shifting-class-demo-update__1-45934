c2c06e59900368ef1a6aee4e0d4ed21823700384
pscbot <pscbot@saas.bg>
2020-09-23 21:42:24 +0300

    Initial commit

---
+++ BitShiftClass/rotateleft.asm
@@ -0,0 +1,19 @@
+; David Fritts corrected by Robert Rayment
+
+[BITS 32]
+
+push ebx
+
+mov eax, dword [esp+0Ch] ; value
+mov ecx, dword [esp+10h] ; shift
+
+mov edx, dword [esp+14h] ; hresult
+
+rol eax, cl
+
+mov dword [edx], eax
+xor eax, eax
+
+pop ebx
+
+ret 16  ' 10 orig ?
